690a5e3a6df89654a36b0240f11a3ff9f0da5ed3
Riley Hilliard <rileyhilliard@gmail.com>
2012-11-26 11:00:49 -0800

    Added RailsForZombies
    Added the RailsForZombies repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsForZombies::Application.config.secret_token = 'c6758191335062e16ec8109aad85f4daa0318e481ab7f27f4e06515e718a4b8a901ab489428f7a356a9efaf3ac235605c05bfe977d6c15e4e54d59f29ea0d788'
